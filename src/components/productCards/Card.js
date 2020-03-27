3a2ff5c1a684d704aec6a47d3065dd439321fdd5
Yehor Lapko <yehorlapko@zoho.com>
2020-03-27 11:50:40 +0200

    added section with product cards

--- src/components/productCards/Card.js
+++ src/components/productCards/Card.js
@@ -3,14 +3,18 @@ import React from 'react';
 export const Card = ({ img, title, category }) => {
     return (
         <div className="card">
-            <img src={img} alt="lingerie" />
+            <img className="card__image" src={img} alt="lingerie" />
             <i className="fas fa-heart"></i>
             <h5 className="card__title">{title}</h5>
             <div className="card__category">
-                <i className="fas fa-map-marker-alt"></i>
-                <p className="category__name">{category}</p>
+                <i className="fas fa-list"></i> 
+                <p className="name">{category}</p>
             </div>
-            <button className="btn">Watch it</button>
+            <div className="card__eco">
+                <i class="fas fa-leaf"></i> 
+                <p className="name">100% natural</p>
+            </div>
+            <button className="btn white">Watch it</button>
         </div>
     )
 }
--- src/components/productCards/Products.js
+++ src/components/productCards/Products.js
@@ -16,7 +16,7 @@ export const Products = () => {
             <Card img={img2} title="Comfortables Panties" category="Slimming panties" />
             <Card img={img3} title="Gorgeous tights" category="Tights" />
             <Card img={img4} title="Like a second skin..." category="Bodysuits" />
-            <Card img={img5} title="Like a dream..." category="Nightgowns" />
+            <Card img={img5} title="Dream-like pajamas" category="Nightgowns" />
             <Card img={img6} title="And other cool stuff" category="Other" />
         </div>
     )
